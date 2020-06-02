f890b2561ead8a5f882272c5849ffdf9c759f1a9
Mario Marin Ramirez <mariomarin502t@gmail.com>
2020-06-02 15:36:43 -0500

    Vigésimo Noveno Commit - Se agregó el codigo para MySQL

--- Proyecto_Final/src/controlador/PacienteDAO.java
+++ Proyecto_Final/src/controlador/PacienteDAO.java
@@ -1,5 +1,12 @@
 package controlador;
 
-public class PacienteDAO {
+import conectorBaseDeDatos.ConexionBD;
+import modelos.Paciente;
 
-}
+public class PacienteDAO {
+	public boolean agregarPaciente(Paciente p) {
+		String sql = "INSERT INTO Pacientes VALUES('"+p.getID_Paciente()+"','"+p.getNombre_Paciente()+"', '"+p.getApPaterno_Paciente()+"','"+p.getApMaterno_Paciente()+"','"+p.getTelefono_Paciente()+"','"+p.getEdad()+"', '"+p.getSexo()+"','"+p.getGravedad_Paciente()+"', '"+p.getCalle()+"', '"+p.getColonia()+"', '"+p.getNumero()+"');";
+		boolean res = new ConexionBD().ejecutarInstruccion(sql); 
+		return res; 
+	}
+}//class
